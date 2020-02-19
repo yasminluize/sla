0d98620dfe6d40bdc7bc63c676d7dad02d119e9a
yasminluize <60760457+yasminluize@users.noreply.github.com>
2020-02-18 21:07:45 -0300

    Add files via upload

---
+++ Calcular1.java
@@ -0,0 +1,18 @@
+package    PacoteJava;
+import javax.swing.*;
+public class Calcular {
+    public static void main(String [] args) {
+        double num1,num2,resp = 0;
+        String aux= "";
+        aux=JOptionPane.showInputDialog(null,"insira");
+        num1= Double.parseDouble(aux);
+        
+         String aux= "";
+        aux=JOptionPane.showInputDialog(null,"insira");
+        num1= Double.parseDouble(aux);
+        
+        resp= num1+num2;
+        
+    }
+    
+}
